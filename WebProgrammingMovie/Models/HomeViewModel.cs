196cadf7d71dfdfd5f9a855e3236a7883a39ea92
Muhammet Cagatay <muhammet.cagatay.mc@gmail.com>
2021-12-20 14:09:08 +0300

    12.20.2021

--- WebProgrammingMovie/Models/HomeViewModel.cs
+++ WebProgrammingMovie/Models/HomeViewModel.cs
@@ -10,12 +10,15 @@ namespace WebProgrammingMovie.Models
         public List<Movie> _TotalMovie { get; set; }
         public List<Movie> _TrendMovie { get; set; }
         public List<Movie> _SliderMovie { get; set; }
+        //categoriye özelse true tüm kategoriler gelirse false
+        public string _categoryName { get; set; }
 
-        public HomeViewModel(List<Movie> TotalMovie, List<Movie> TrendMovie, List<Movie> SliderMovie)
+        public HomeViewModel(List<Movie> TotalMovie, List<Movie> TrendMovie, List<Movie> SliderMovie,string categoryName)
         {
             _TotalMovie = TotalMovie;
             _TrendMovie = TrendMovie;
             _SliderMovie = SliderMovie;
+            _categoryName = categoryName;
         }
     }
 }
