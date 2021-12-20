e577360ff92506d42668602729e051e5688b38a9
Muhammet Cagatay <muhammet.cagatay.mc@gmail.com>
2021-12-20 23:01:13 +0300

    12.20.2021

---
+++ WebProgrammingMovie/Models/MovieRating.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProgrammingMovie.Models
+{
+    public class MovieRating
+    {
+        public MovieRating(Movie movie,Rating rating)
+        {
+            _Movie = movie;
+            _Rating = rating;
+            
+        }
+        public Movie _Movie { get; set; }
+        public Rating _Rating { get; set; }
+    }
+}
